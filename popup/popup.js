53f31b67abb7e5a7613b1b9160f495ee7245b9f1
mingpepe <mingpepemath@gamil.com>
2022-10-06 22:37:56 +0800

    Modify log

--- popup/popup.js
+++ popup/popup.js
@@ -2,13 +2,13 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var url = tabs[0].url;
     if (url.includes("microsoft.com") && url.includes("zh-tw"))
     {
-        console.log("Redirect!!");
+        console.log("Redurect this page");
         var new_url = url.replace("zh-tw", "en-us");
         chrome.tabs.update(tabs[0].id, {url: new_url});
     }
     else
     {
-        console.log("Do not need to redirect current page");
+        console.log("Do not need to redirect this page");
     }
   });
 
