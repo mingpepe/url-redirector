aa8057facaa4846f3aa0518c282fa36c0970624d
mingpepe <mingpepemath@gamil.com>
2022-10-06 22:38:30 +0800

    Auto format the code

--- popup/popup.js
+++ popup/popup.js
@@ -1,15 +1,12 @@
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var url = tabs[0].url;
-    if (url.includes("microsoft.com") && url.includes("zh-tw"))
-    {
+    if (url.includes("microsoft.com") && url.includes("zh-tw")) {
         console.log("Redurect this page");
         var new_url = url.replace("zh-tw", "en-us");
-        chrome.tabs.update(tabs[0].id, {url: new_url});
+        chrome.tabs.update(tabs[0].id, { url: new_url });
     }
-    else
-    {
+    else {
         console.log("Do not need to redirect this page");
     }
-  });
+});
 
-  
