0666d74a06b89cf0843c4a8e2fc9fe89151ebea9
mingpepe <mingpepemath@gmail.com>
2022-11-23 23:05:06 +0800

    Modify console log

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,12 @@
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var url = tabs[0].url;
     if (url.includes("microsoft.com") && url.includes("zh-tw")) {
-        console.log("Redurect this page");
+        console.log("Redirect MSDN page");
         var new_url = url.replace("zh-tw", "en-us");
         chrome.tabs.update(tabs[0].id, { url: new_url });
     }
     else if (url.includes("https://developer.mozilla.org/") && url.includes("zh-TW")) {
-        console.log("Redurect this page");
+        console.log("Redirect MDN page");
         var new_url = url.replace("zh-TW", "en-US");
         chrome.tabs.update(tabs[0].id, { url: new_url });
     }
